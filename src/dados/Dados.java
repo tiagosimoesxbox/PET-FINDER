7ce5fd1212059cbcc23143e8a2976be90c553ff0
tiago <tiago@192.168.1.5>
2018-11-19 21:54:46 +0000

    commit dados finalizada

--- src/dados/Dados.java
+++ src/dados/Dados.java
@@ -1,16 +1,24 @@
 
 package dados;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
 import modelo.Adotante;
 import modelo.Animal;
 import modelo.Doador;
 
-public class Dados {
+public class Dados implements Serializable {
     
     private ArrayList<Animal> animais;
     private ArrayList<Doador> doadores;
     private ArrayList<Adotante> adotantes;
+    
+    public Dados() {
+        animais = new ArrayList<>();
+        doadores = new ArrayList<>();
+        adotantes = new ArrayList<>();
+    }
     
     public ArrayList<Animal> getAnimais() {
         return animais;
@@ -32,17 +40,67 @@ public class Dados {
         adotantes.add(a);
     }
     
-    public void getAnimal(int pos) {
-        animais.get(pos);
+    public Animal getAnimalComId(int id) {
+        Iterator it = animais.iterator();
+        
+        while (it.hasNext()) {
+            Animal a = (Animal) it.next();
+            if (a.getId() == id)
+                return a;
+        }
+        
+        return null;
     }
-    public void adicionaDoador(int pos) {
-        doadores.get(pos);
+    public Doador getDoadorComId(int id) {
+        Iterator it = doadores.iterator();
+        
+        while (it.hasNext()) {
+            Doador a = (Doador) it.next();
+            if (a.getId() == id)
+                return a;
+        }
+        
+        return null;
     }
-    public void adicionaAdotante(int pos) {
-        adotantes.get(pos);
+    public Adotante getAdotanteComId(int id) {
+        Iterator it = adotantes.iterator();
+        
+        while (it.hasNext()) {
+            Adotante a = (Adotante) it.next();
+            if (a.getId() == id)
+                return a;
+        }
+        
+        return null;
     }
     
     // REEDITAR e REMOVER
+    public void removerAnimalComId(int id) {
+        for (Animal a: animais) {
+            if (a.getId() == id) {
+                animais.remove(a);
+                return;
+            }
+        }
+    }
+
+    public void removerDoadorComId(int id) {
+        for (Doador a: doadores) {
+            if (a.getId() == id) {
+                doadores.remove(a);
+                return;
+            }
+        }
+    }
+
+    public void removerAdotanteComId(int id) {
+        for (Adotante a: adotantes) {
+            if (a.getId() == id) {
+                adotantes.remove(a);
+                return;
+            }
+        }
+    }
     
     
 }
