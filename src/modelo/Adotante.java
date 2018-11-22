6694751b73c673ae86328bb2a78a0ce287e0f2d5
jpval <jpval@10.65.142.100>
2018-11-22 15:53:15 +0000

    commit lnaçamento da exceção do Adotante

--- src/modelo/Adotante.java
+++ src/modelo/Adotante.java
@@ -12,11 +12,31 @@ public class Adotante implements Registo, Serializable{
     private String morada;
     private int contacto;
     
-    public Adotante(final String nome, final String morada, final int contacto) {
+    public Adotante(final String nome, final String morada, final int contacto) throws Exception{
+        
+        if(nome.length() < 1 || nome.length() > 30)
+            throw new Exception("Nome inválido. Limite de caracteres entre 1 e 30.");
+        if(morada.length() < 1 || morada.length() > 30)
+            throw new Exception("Nome de animal inválido. Limite de caracteres entre 1 e 30.");
+        if(contaDigitos(contacto) != 9)
+            throw new Exception("Numero invalido.");
+        
         id = ADOTANTES_ID++;
         this.nome = nome;
         this.morada = morada;
         this.contacto = contacto;
+    }
+    
+    private int contaDigitos(int num){
+        
+        int cont = 1;
+        
+        while(num / 10 != 0){
+            num=num/10;
+            cont++;
+        }
+        
+        return cont;
     }
     
     public void reeditarAdotante(final String nome, final String morada, final int contacto) {
